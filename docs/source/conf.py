cbd295ea3eb323e08deca78accc2197ec1bb4e15
Sebastian Havens <sebastian.havens@outlook.com>
2024-10-28 13:07:59 +0000

    rtd files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'pymatnest-post'
+#copyright = '2021, Graziella'
+author = 'Sebastian Havens'
 
 release = '0.1'
 version = '0.1.0'
