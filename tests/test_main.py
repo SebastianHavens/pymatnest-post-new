eced8604ac5978c326cf62b0add210cb2d0f2cc2
Sebastian Havens <sebastian.havens@outlook.com>
2024-10-29 14:56:37 +0000

    Modification to tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,7 +8,7 @@ import glob
 @pytest.fixture
 def mock_sys_argv():
     original_argv = sys.argv.copy()
-    sys.argv = ['program', '-qw', '-rdf', '-M', '100', '-n', '10', '-D', '5']
+    sys.argv = ['program', '--qw', '--rdf', '-M', '100', '-n', '10', '-D', '5']
     yield # Temporarily use the mock argv
     sys.argv = original_argv  # Restore the original argv after the test
 
@@ -65,7 +65,7 @@ def test_calculate_partition_function_primary_path(mock_sys_argv):
 
             # Check if `subprocess.run` was called with the correct parameters
             mock_subproc_run.assert_called_once_with(
-                ['ns_analyse', 'test.energies', '-M', '100.0', '-n', '10', '-D', '5.0'],
+                ['ns_analyse', 'test.energies', '-M', '100.0', '-n', '10', '-D', '5.0', '-k', '8.6173324e-05'],
                 stdout=mocked_file(),
                 check=True
             )
@@ -90,43 +90,43 @@ def test_calculate_partition_function_fallback_to_local(mock_sys_argv):
 
             # Check if it was called twice with the correct fallback
             expected_calls = [
-                call(['ns_analyse', 'test.energies', '-M', '100.0', '-n', '10', '-D', '5.0'], stdout=mocked_file(), check=True),
-                call(['./ns_analyse', 'test.energies', '-M', '100.0', '-n', '10', '-D', '5.0'], stdout=mocked_file(), check=True)
+                call(['ns_analyse', 'test.energies', '-M', '100.0', '-n', '10', '-D', '5.0', '-k', '8.6173324e-05'], stdout=mocked_file(), check=True),
+                call(['./ns_analyse', 'test.energies', '-M', '100.0', '-n', '10', '-D', '5.0', '-k', '8.6173324e-05'], stdout=mocked_file(), check=True)
             ]
             mock_subproc_run.assert_has_calls(expected_calls)
 
 
-def test_read_in_num_of_trajectories_max_number(monkeypatch, mock_sys_argv):
-    """
-    Test to ensure read_in_number_of_trajectories_max_number returns the correct number
-     Args:
-         mock_sys_argv: User input as arguments for Config initialisation
-     """
-    config = Config()
-    config.prefix = 'test'
-    # Mock the file list in the current directory
-    monkeypatch.setattr('os.listdir', lambda _: [
-        'test.traj.0.extxyz', 'test.traj.1.extxyz', 'test.traj.2.extxyz',
-        'test.traj.3.extxyz', 'test.traj.8.extxyz'
-    ])
-    config.read_in_num_of_trajectories()
-    # Check if the function returns the highest number for base name 'test'
-    assert config.num_of_trajectories == 5
-
-
-def test_read_in_num_of_trajectories_no_match(monkeypatch, mock_sys_argv):
-    """
-    Test to ensure read_in_number_of_trajectories_max_number returns none when no file matches.
-     Args:
-         mock_sys_argv: User input as arguments for Config initialisation
-     """
-
-    config = Config()
-    config.prefix = 'test'
-    # Mock a list with no matching files
-    monkeypatch.setattr('os.listdir', lambda _: [
-        'example.traj.0.extxyz', 'example.traj.1.extxyz'
-    ])
-    config.read_in_num_of_trajectories()
-    # Check if the function returns None when no files match 'test'
-    assert config.num_of_trajectories is None
+# def test_read_in_num_of_trajectories_max_number(monkeypatch, mock_sys_argv):
+#     """
+#     Test to ensure read_in_number_of_trajectories_max_number returns the correct number
+#      Args:
+#          mock_sys_argv: User input as arguments for Config initialisation
+#      """
+#     config = Config()
+#     config.prefix = 'test'
+#     # Mock the file list in the current directory
+#     monkeypatch.setattr('os.listdir', lambda _: [
+#         'test.traj.0.extxyz', 'test.traj.1.extxyz', 'test.traj.2.extxyz',
+#         'test.traj.3.extxyz', 'test.traj.8.extxyz'
+#     ])
+#     config.read_in_num_of_trajectories()
+#     # Check if the function returns the highest number for base name 'test'
+#     assert config.num_of_trajectories == 5
+#
+#
+# def test_read_in_num_of_trajectories_no_match(monkeypatch, mock_sys_argv):
+#     """
+#     Test to ensure read_in_number_of_trajectories_max_number returns none when no file matches.
+#      Args:
+#          mock_sys_argv: User input as arguments for Config initialisation
+#      """
+#
+#     config = Config()
+#     config.prefix = 'test'
+#     # Mock a list with no matching files
+#     monkeypatch.setattr('os.listdir', lambda _: [
+#         'example.traj.0.extxyz', 'example.traj.1.extxyz'
+#     ])
+#     config.read_in_num_of_trajectories()
+#     # Check if the function returns None when no files match 'test'
+#     assert config.num_of_trajectories is None
